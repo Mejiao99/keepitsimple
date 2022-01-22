ea1096e8ba9ccb2af530b2fdbf0c05a8ff202f1d
Mejiao <Jonathan19991@gmail.com>
2022-01-21 19:12:59 -0500

    Initial commit

--- src/main/java/com/mejiao/keepitsimple/common/tile/TileEnergySimple.java
+++ src/main/java/com/mejiao/keepitsimple/common/tile/TileEnergySimple.java
@@ -1,7 +1,8 @@
 package com.mejiao.keepitsimple.common.tile;
 
-import com.mejiao.keepitsimple.common.integration.energy.forgeenergy.SimpleEnergyStorage;
-import net.minecraft.tileentity.ITickableTileEntity;
+
+import com.mejiao.keepitsimple.common.integration.energy.storage.SimpleEnergyStorage;
+import net.minecraft.client.renderer.texture.ITickable;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.Direction;
@@ -12,8 +13,8 @@ import net.minecraftforge.energy.CapabilityEnergy;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public class TileEnergySimple extends TileEntity implements ITickableTileEntity {
-    private final SimpleEnergyStorage storage = new SimpleEnergyStorage(5000);
+public class TileEnergySimple extends TileEntity implements ITickable {
+    private final SimpleEnergyStorage storage = new SimpleEnergyStorage(5000, 2000, 1000);
     private final LazyOptional<SimpleEnergyStorage> energyStorage = LazyOptional.of(() -> storage);
 
 
@@ -25,32 +26,54 @@ public class TileEnergySimple extends TileEntity implements ITickableTileEntity 
         this(TileSimpleEntities.SIMPLE_ENERGY_TILE.get());
     }
 
+
+
+
     @Nonnull
     @Override
     public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
         System.out.println("Side1: " + side);
 
         if (cap == CapabilityEnergy.ENERGY) {
-            energyStorage.cast();
+            boolean b = true;
+            LazyOptional<Boolean> booleanLazyOptional = LazyOptional.of(() -> b);
+            return booleanLazyOptional.cast();
         }
 
-        System.out.println("Side2 : " + side);
-        return super.getCapability(cap, side);
+        System.out.println("defaultprr: " + cap.getDefaultInstance());
+
+
+        return energyStorage.cast();
+
+        //return super.getCapability(cap, side);
     }
 
+    @Nonnull
+    @Override
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap) {
+        if (cap == CapabilityEnergy.ENERGY) {
+            boolean b = true;
+            LazyOptional<Boolean> booleanLazyOptional = LazyOptional.of(() -> b);
+            return booleanLazyOptional.cast();
+        }
+        return super.getCapability(cap);
+    }
 
     @Override
     public void tick() {
 
         if (storage.getEnergyStored() < storage.getMaxEnergyStored()) {
 
-            this.storage.receiveEnergy(100, false);
+            this.storage.receiveEnergy(1500, false);
 
         }
 
+        if (storage.getMaxEnergyStored() == 5000) {
+        }
 
         System.out.println("getEnergyStored: " + storage.getEnergyStored());
         System.out.println("getMaxEnergyStored: " + storage.getMaxEnergyStored());
+
         System.out.println("TileEnergySimple.tick");
 
     }
