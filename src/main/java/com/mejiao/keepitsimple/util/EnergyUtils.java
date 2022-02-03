cea6d3a59e01200e36d8ad3e9837f9a0cdddc94c
Mejiao <Jonathan19991@gmail.com>
2022-02-03 13:49:36 -0500

    IEnergyHandler deleted

--- src/main/java/com/mejiao/keepitsimple/util/EnergyUtils.java
+++ src/main/java/com/mejiao/keepitsimple/util/EnergyUtils.java
@@ -1,14 +1,11 @@
 package com.mejiao.keepitsimple.util;
 
-import com.mejiao.keepitsimple.common.block.IEnergyHandler;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorldReader;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.energy.CapabilityEnergy;
-import net.minecraftforge.energy.EnergyStorage;
 import net.minecraftforge.energy.IEnergyStorage;
 
 import javax.annotation.Nullable;
@@ -16,32 +13,33 @@ import javax.annotation.Nullable;
 public final class EnergyUtils {
     private EnergyUtils() {throw new IllegalAccessError("Utility class");}
 
-    public static void trySendToNeighbors(IBlockReader world, BlockPos pos, IEnergyHandler energyHandler, int maxSend) {
-        for (Direction side : Direction.values()) {
-            if (energyHandler.getEnergyStored() == 0) {
-                return;
-            }
-            trySendTo(world, pos, energyHandler, maxSend, side);
-        }
-    }
 
-    public static void trySendTo(IBlockReader world, BlockPos pos, IEnergyHandler energyHandler, int maxSend, Direction side) {
-        TileEntity tileEntity = world.getBlockEntity(pos.relative(side));
-        if (tileEntity != null) {
-            IEnergyStorage energy = energyHandler.getEnergy(side).orElse(new EnergyStorage(0));
-            tileEntity.getCapability(CapabilityEnergy.ENERGY, side.getOpposite()).ifPresent(other -> trySendEnergy(maxSend, energy, other));
-        }
-    }
-
-    private static void trySendEnergy(int maxSend, IEnergyStorage energy, IEnergyStorage other) {
-        if (other.canReceive()) {
-            int toSend = energy.extractEnergy(maxSend, true);
-            int sent = other.receiveEnergy(toSend, false);
-            if (sent > 0) {
-                energy.extractEnergy(sent, false);
-            }
-        }
-    }
+//    public static void trySendToNeighbors(IBlockReader world, BlockPos pos, IEnergyHandler energyHandler, int maxSend) {
+//        for (Direction side : Direction.values()) {
+//            if (energyHandler.getEnergyStored() == 0) {
+//                return;
+//            }
+//            trySendTo(world, pos, energyHandler, maxSend, side);
+//        }
+//    }
+//
+//    public static void trySendTo(IBlockReader world, BlockPos pos, IEnergyHandler energyHandler, int maxSend, Direction side) {
+//        TileEntity tileEntity = world.getBlockEntity(pos.relative(side));
+//        if (tileEntity != null) {
+//            IEnergyStorage energy = energyHandler.getEnergy(side).orElse(new EnergyStorage(0));
+//            tileEntity.getCapability(CapabilityEnergy.ENERGY, side.getOpposite()).ifPresent(other -> trySendEnergy(maxSend, energy, other));
+//        }
+//    }
+//
+//    private static void trySendEnergy(int maxSend, IEnergyStorage energy, IEnergyStorage other) {
+//        if (other.canReceive()) {
+//            int toSend = energy.extractEnergy(maxSend, true);
+//            int sent = other.receiveEnergy(toSend, false);
+//            if (sent > 0) {
+//                energy.extractEnergy(sent, false);
+//            }
+//        }
+//    }
 
     /**
      * Gets the energy capability for the block at the given position. If it does not have an energy
